cc120cd1c7435cf9e58cc32bbc6cdaf5e212f649
chetanareddy28 <chetanam54@gmail.com>
2024-08-25 12:20:22 +0530

    added implementation for add method

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,9 @@
-class Calculator
-{
-public static void main(String args[])
-{
-add(10,20);
-}
+class Calculator {
+    public static void main(String args[]) {
+        add(10, 20);
+    }
+
+    static void add(int a, int b) {
+        System.out.println("Summation of two numbers is:" + (a + b));
+    }
 }
